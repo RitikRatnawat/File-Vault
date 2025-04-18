80b31a64131c86717def41537528b62a3a3f7557
RitikRatnawat <ritikratnawat786@gmail.com>
2025-04-19 00:57:06 +0530

    Added Serializers for Data Models

--- backend/files/serializers.py
+++ backend/files/serializers.py
@@ -1,8 +1,34 @@
 from rest_framework import serializers
-from .models import File
+from .models import File, StorageStatistics
 
 class FileSerializer(serializers.ModelSerializer):
+    """Serializer for File model"""
+    
+    is_duplicate = serializers.SerializerMethodField()
+    original_filename = serializers.SerializerMethodField()
+
     class Meta:
         model = File
-        fields = ['id', 'file', 'original_filename', 'file_type', 'size', 'uploaded_at']
-        read_only_fields = ['id', 'uploaded_at'] 
+        fields = ['id', 'name', 'file', 'file_type', 'size', 'uploaded_at', 'is_duplicate', 'original_filename']
+        read_only_fields = ['id', 'uploaded_at']
+        
+    
+    def get_is_duplicate(self, obj):
+        """Check if the file is a duplicate"""
+        
+        return obj.original_file is not None
+    
+    
+    def get_original_filename(self, obj):
+        """Get the original filename if the file is a duplicate"""
+        
+        return obj.original_file.name or None
+    
+
+class StorageStatisticsSerializer(serializers.ModelSerializer):
+    """Serializer for StorageStatistics model"""
+    
+    class Meta:
+        model = StorageStatistics
+        fields = ['total_files', 'unique_files', 'duplicates', 'total_size', 'saved_size', 'last_updated']
+        read_only_fields = ['last_updated']
